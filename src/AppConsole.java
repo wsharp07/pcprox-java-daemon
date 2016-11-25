876db33f4b9894b490b6062f0f0ec06855111ae7
William Sharp <wsharp@ltu.edu>
2016-11-25 01:01:55 -0500

    initial pass at a mongodb integration and service

--- src/AppConsole.java
+++ src/AppConsole.java
@@ -1,11 +1,30 @@
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.DB;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
 import org.apache.commons.daemon.DaemonContext;
 import org.apache.commons.daemon.DaemonController;
+
+import java.net.UnknownHostException;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 public class AppConsole {
     private static App _app;
     public static void main(String[] args) {
 
-        Runtime.getRuntime().addShutdownHook( new Thread() {
+
+        try {
+            MongoService mongoService = new MongoService("pcprox");
+            System.out.println("Connected? " + mongoService.IsConnected());
+
+            mongoService.setCollection("card_reads");
+            mongoService.Insert(createDBObject(new Date(), "4D0029418248"));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        /*Runtime.getRuntime().addShutdownHook( new Thread() {
             @Override
             public void run() {
                 System.out.println("Application shutdown");
@@ -24,7 +43,14 @@ public class AppConsole {
             _app.start();
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        } */
+    }
+
+    private static DBObject createDBObject(Date cardSwipeDate, String cardId) {
+        BasicDBObjectBuilder docBuilder = BasicDBObjectBuilder.start();
+        docBuilder.append("cardSwipeDate", cardSwipeDate);
+        docBuilder.append("cardId", cardId);
+        return docBuilder.get();
     }
 }
 
